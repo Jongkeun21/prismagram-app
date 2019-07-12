1be00d4b1a7491244588f5e3af8a87a30c4d2f40
[Jongkeun21] <[whdrms5560@gmail.com]>
2019-07-12 16:22:26 +0900

    prismagram-app

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading, Font, Asset } from "expo";
+import { Text, View, AsyncStorage } from "react-native";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import { ApolloProvider } from "react-apollo-hooks";
+import options from "./apollo";
+import styles from "./styles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      await Asset.loadAsync([require("./assets/Instagram.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage
+      });
+      const client = new ApolloClient({
+        cache,
+        ...options
+      });
+      setLoaded(true);
+      setClient(client);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return loaded && client
+    ? <ApolloProvider client={client}>
+        <ThemeProvider theme={styles}>
+          <View>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </ThemeProvider>
+      </ApolloProvider>
+    : <AppLoading />;
+}
---
+++ styles.js
@@ -0,0 +1,9 @@
+export default {
+  blackColor: "#262626",
+  greyColor: "#F9F9F9",
+  derkGreyColor: "#999",
+  lightGreyColor: "#C7C7C7",
+  redColor: "#ED4956",
+  blueColor: "#3897F0",
+  darkBlueColor: "#003569"
+};
